ec12e2a55d800cd40bb1921db781b8e1088e8489
Kelsey OConnell <kelseyoconnell2217@gmail.com>
2024-02-14 15:58:15 -0500

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Add your code here
+function submitData(submittedName, submittedEmail){
+    const configObject = {
+        method: 'POST',
+        headers: {
+            'Content-Type' : "application/json",
+            'Accept' : "application/json"
+        },
+        body: JSON.stringify({
+            name : submittedName,
+            email : submittedEmail
+        })}
+    return fetch("http://localhost:3000/users", configObject)
+        .then(function(resp){
+            return resp.json()})
+        .then(data => {
+            const body = document.querySelector('body');
+            body.append(data.id)})
+        .catch(error => {
+            const body = document.querySelector('body');
+            body.append(error.message)})
+        }
+        
+    
